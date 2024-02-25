db26102b4c489ca98f9754a183d59bd165f8fd56
Dimitris Zerdevas <42102327+dimzerdevas@users.noreply.github.com>
2024-02-25 15:38:43 +0200

    init commit for ajax request

--- handleRequest.js
+++ handleRequest.js
@@ -2,3 +2,30 @@ import { renderPokemonCards } from "./helpers.js";
 
 // Setting our API url
 const pokeAPI = "https://localhost:3000/pokemon";
+
+// Creating a new request
+function handleResponse() {
+  const { results } = JSON.parse(this.response);
+  appendPokemonElements(results);
+}
+
+const request = new XMLHttpRequest();
+
+request.addEventListener("load", handleResponse);
+
+// method open() https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
+
+// Definition: Initializes a newly-created request, or re-initializes an existing one.
+// Parameters:
+//      open(
+//          HTTP Request Method,    * required: "GET", "POST", "PUT", "DELETE"
+//          url,                    * required: URL of the resource to send the request to
+//          Async Boolean,          optional: defaults to null
+//          user,                   optional: defaults to null
+//          password                optional: defaults to null
+//          )
+
+request.open("GET", pokeAPI, true);
+
+// Send the request
+request.send();
