befb4f2fefbd9e63d88e73d1a5b089c4260bd721
Dimitris Zerdevas <42102327+dimzerdevas@users.noreply.github.com>
2024-02-28 21:49:06 +0200

    implemented fetch with es6 and old way

--- handleRequest.js
+++ handleRequest.js
@@ -2,3 +2,20 @@ import { renderPokemonCards } from "./helpers.js";
 
 // Setting our API url
 const pokeAPI = "https://localhost:3000/pokemon";
+
+async function handleRequest() {
+  const response = await fetch(pokeAPI);
+  const pokemonData = await response.json();
+  renderPokemonCards(pokemonData.results);
+}
+
+handleRequest();
+
+// Chaining
+function oldHandleRequest() {
+  fetch(pokeAPI)
+    .then((res) => res.json())
+    .then((pokemonData) => renderPokemonCards(pokemonData.results));
+}
+
+oldHandleRequest();
